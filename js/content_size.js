84b72b32c4f1ddde373dc782d42ecabef7428921
Ivo Liba <ivo.liba@student.tuke.sk>
2024-11-06 20:36:21 +0100

    Sekcia zmena velkosti pisma

--- js/content_size.js
+++ js/content_size.js
@@ -3,19 +3,19 @@
 
 // Funkcia na zmenu velkosti pisma
 function changeFontSize(factor) {
-    const body = document.body; // Target the entire body element
-    // Get the current font size of the body
+    const body = document.body; // Cele Body element
+    // dostat aktualnu velkost pisma
     const currentFontSize = window.getComputedStyle(body).fontSize;
     const newSize = parseFloat(currentFontSize) + factor;
     body.style.fontSize = `${newSize}px`;
 }
 
-// funkčnosť tlačidiel na zväčšenie alebo zmenšenie veľkosti písma
+// funkcnosť tlacidiel na zvacsenie alebo zmensenie velkosti pisma
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('increase-font').addEventListener('click', () => changeFontSize(2));
     document.getElementById('decrease-font').addEventListener('click', () => changeFontSize(-2));
 
-    // funkčnosť možnosti Ctrl + šípka hore/dole
+    //Ctrl + Sipky
     document.addEventListener('keydown', (event) => {
 
         if (event.ctrlKey && event.key === 'ArrowUp') {
@@ -26,33 +26,21 @@ document.addEventListener('DOMContentLoaded', () => {
         } else if (event.ctrlKey && event.key === 'ArrowDown') {
             // Ctrl + sipka dole pre zmensenie pisma
             changeFontSize(-2);
-            event.preventDefault(); // Prevent default browser behavior
+            event.preventDefault();
         }
     });
-    document.addEventListener('keydown', (event) => {
 
-        if (event.ctrlKey && event.key === 'ArrowUp') {
-            // Ctrl + sipka hore pre zvacsenie pisma
-            changeFontSize(2);
-            event.preventDefault(); // Prevent default browser behavior
-
-        } else if (event.ctrlKey && event.key === 'ArrowDown') {
-            // Ctrl + sipka dole pre zmensenie pisma
-            changeFontSize(-2);
-            event.preventDefault(); // Prevent default browser behavior
-        }
-    });
-    // Shift + koliesko myši pre zmenu velkosti pisma
+    // Shift + koliesko myši
     document.addEventListener('wheel', (event) => {
         if (event.shiftKey) { // Check if the Shift key is pressed
             if (event.deltaY < 0) {
-                // Scrolling up, increase font size
+                // Skrollovanie hore zvacsuje velkost
                 changeFontSize(2);
             } else if (event.deltaY > 0) {
-                // Scrolling down, decrease font size
+                // Skrollovanie dole zmensuje velkost
                 changeFontSize(-2);
             }
-            event.preventDefault(); // Prevent default scrolling behavior
+            event.preventDefault();
         }
     });
 });
